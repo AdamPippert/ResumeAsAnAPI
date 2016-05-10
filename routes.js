01fe558a13ec5573b2a9f9c4a9b58766b7415cc6
EC2 Default User <ec2-user@ip-172-31-40-35.us-west-2.compute.internal>
2016-05-10 22:36:06 +0000

    fixed routes.js

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 module.exports = function(app){
-    var musicians = require('./controllers/resumes.js');
-    app.get('/resumes', resume.findAll);
-    app.get('/resumes/:id', resume.findById);
-    app.post('/resumes', resume.add);
-    app.put('/resumes/:id', resume.update);
-    app.delete('/resumes/:id', resume.delete);
+    var resumes = require('./controllers/resumes.js');
+    app.get('/resumes', resumes.findAll);
+    app.get('/resumes/:id', resumes.findById);
+    app.post('/resumes', resumes.add);
+    app.put('/resumes/:id', resumes.update);
+    app.delete('/resumes/:id', resumes.delete);
 }
