f576a4c9e0f022b76d8a5d01dd31cd1b8e06959d
EC2 Default User <ec2-user@ip-172-31-40-35.us-west-2.compute.internal>
2016-05-10 22:40:58 +0000

    added basic routing to app.js

--- app.js
+++ app.js
@@ -3,16 +3,6 @@ var app = express();
 
 require('./routes')(app);
 
-app.get('/resumes/:name', function(req, res) {
-
-    // Get /resumes/AdamPippert
-    console.log(req.params.name)
-    // => Adam
-
-    res.send('{"id": 1,"name":"AdamPippert"}');
-});
-
-
 app.get('/', function(req, res) {
     res.send('Resume As An API\nTo use, add /resumes/name for a resume (e.g. "/resumes/AdamPippert")');
 });
