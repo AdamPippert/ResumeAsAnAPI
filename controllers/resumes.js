648f7a3d30e37ea1a5b4c8a7697fc8859e5a42b0
EC2 Default User <ec2-user@ip-172-31-40-35.us-west-2.compute.internal>
2016-05-11 05:37:00 +0000

    API functionality added and functionality commented

--- controllers/resumes.js
+++ controllers/resumes.js
@@ -2,12 +2,72 @@ require('../models/resume.js');
 var mongoose = require('mongoose'),
     Resume = mongoose.model('Resume');
 
+// * API FUNCTIONS *
+
+// ----------------
+// * BEGIN import
+// import function loads the sample resume file.  To add more resumes, use the add function.
+var firstRes;
+$.getJSON('sampleresume.json', function(response){
+    firstRes = response;
+    alert(firstRes.property);
+})
+
+exports.import = function(req, res){
+    Resume.create(
+	firstRes, function (err) {
+	    if (err) return console.log(err);
+	    return res.send(202);
+	});
+};
+// * END import
+
+// ----------------
+// * BEGIN findAll
+// findAll posts all of the resume entries in the database.
 exports.findAll = function(req, res){
     Resume.find({},function(err, results) {
 	return res.send(results);
     });
 };
+// * END findAll
+
+// ----------------
+// * BEGIN findById
+// findById will find a particular entry according to its database ID.
 exports.findById = function() {};
-exports.add = function() {};
-exports.update = function() {};
-exports.delete = function() {};
+
+exports.add = function(req, res) {
+    Resume.create(req.body, function (err, resume) {
+	if (err) return console.log(err);
+	return res.send(resume);
+    });
+}
+// * END findByID
+
+// ----------------
+// * BEGIN update
+// update allows a request to send specific data changes to an entry
+exports.update = function(req, res) {
+    var id = req.params.id;
+    var updates = req.body;
+
+    Resume.update({"_id":id}, req.body,
+		    function (err, numberAffected) {
+			if (err) return console.log(err);
+			console.log('Updated %d resumes', numberAffected);
+			res.send(202);
+		    });
+}
+// * END update
+
+// ----------------
+// * BEGIN delete
+// delete removes an entry based on id number
+exports.delete = function(req, res){
+    var id = req.params.id;
+    Resume.remove({'_id':id},function(result) {
+	return res.send(result);
+    });
+};
+// END delete
